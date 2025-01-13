d3322b4274162fdd7a1426e309e9f197a71ad8bf
AnthonySTZ <anthonycandy2309@gmail.com>
2025-01-13 10:34:30 +0100

    get_selected_objects

--- src/clean_combine.py
+++ src/clean_combine.py
@@ -3,3 +3,10 @@ import maya.cmds as cmds  # type: ignore
 
 def clean_combine() -> None:
     pass
+
+
+def get_selected_objects() -> list[str]:
+    selection = cmds.ls(sl=True, o=True)
+    if len(selection) < 2:
+        raise RuntimeError("Please select at least two objects")
+    return selection
