4b9943356ead6ef4376582c93a7f86b8e578f52a
AnthonySTZ <anthonycandy2309@gmail.com>
2025-01-13 18:34:18 +0100

    center objects

--- src/maya_handler/center_objects.py
+++ src/maya_handler/center_objects.py
@@ -15,4 +15,16 @@ def get_selected_objects() -> list[str]:
 
 
 def center_obj(object: str) -> None:
-    pass
+    obj_pos = get_obj_position_in_worldspace(object)
+    cmds.move(
+        -obj_pos[0], -obj_pos[1], -obj_pos[2], object, a=True, ls=True, r=True
+    )  # center object
+    freeze_transform(object)
+
+
+def get_obj_position_in_worldspace(object: str) -> list[float]:
+    return list(cmds.getAttr(object + ".rotatePivot")[0])
+
+
+def freeze_transform(object: str) -> None:
+    cmds.makeIdentity(object, a=True)
