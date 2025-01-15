ae7373e8fdb2e03fb24f45b58db012be08164617
AnthonySTZ <anthonycandy2309@gmail.com>
2025-01-15 17:10:28 +0100

    select base face

--- src/maya_handler/select_contained_faces.py
+++ src/maya_handler/select_contained_faces.py
@@ -1,10 +1,16 @@
 import maya.cmds as cmds  # type: ignore
 from importlib import reload
-from maya_handler import selection
+from maya_handler import selection, logics
 
 reload(selection)
+reload(logics)
 
 
 def select_contained_faces():
-    edges = selection.get_selected_edges()
-    print(edges)
+    selected_obj = selection.get_selected_objects()[0]
+    selected_edges = selection.get_selected_objects(flatten=True)
+    faces = cmds.polyListComponentConversion(
+        selected_edges[0], fe=True, tf=True, bo=True
+    )
+    base_face_number = logics.extract_faces_from_selection(faces)[0]
+    selection.select_faces_from(selected_obj, [base_face_number])
